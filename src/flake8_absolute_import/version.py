98c63785e7ff08d1622be2bee72ef8e9dbb563d3
Brian Skinn <brian.skinn@gmail.com>
2021-12-04 19:11:40 -0500

    Release changes
    Update CHANGELOG
    Update copyright end years
    Fully remove Python 3.5 from tox

--- src/flake8_absolute_import/version.py
+++ src/flake8_absolute_import/version.py
@@ -9,7 +9,7 @@ flake8 plugin to require absolute imports
     6 Sep 2019
 
 **Copyright**
-    \(c) Brian Skinn 2019-2020
+    \(c) Brian Skinn 2019-2021
 
 **Source Repository**
     http://github.com/bskinn/flake8-absolute-import
@@ -21,4 +21,4 @@ flake8 plugin to require absolute imports
 
 """
 
-__version__ = "1.0.1.dev0"
+__version__ = "1.0.0.1"
